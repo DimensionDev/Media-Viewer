6ab7244ed97746e1148980da09b44185d026483f
zhouhanseng <z308114274@gmail.com>
2022-01-06 14:52:26 +0800

    chore: always use cors proxy

--- src/viewer.ts
+++ src/viewer.ts
@@ -11,20 +11,16 @@ declare global {
 
 export async function onView(options: ViewerOptions): Promise<Element | null> {
   let type = options.type
+
+  options.url = prepareURL(`${CORS_PROXY}/?${encodeURIComponent(options.url)}`)
+
   if (!type) {
     try {
       type = (await getContentType(options.url)) ?? ''
     } catch (error) {
-      if (options.url.startsWith(CORS_PROXY)) {
-        throw error
-      }
-      return onView({
-        ...options,
-        url: `${CORS_PROXY}/?${encodeURIComponent(options.url)}`,
-      })
+      throw error
     }
   }
-  options.url = prepareURL(options.url)
   const { pathname } = new URL(options.url)
   if (options.source === 'erc721') {
     return onERC721(options)
